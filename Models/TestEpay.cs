7fe723f3b31f63866ab975ed969307e92e5c5796
Sahitya Kotha <sahityakotha@Sahityas-MBP.fritz.box>
2023-06-03 18:14:52 +0200

    commit

---
+++ Models/TestEpay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpayTest.Models
+{
+    public class TestEpay
+    {
+        public string UserInput { get; set; }
+        public string UserOutPut { get; set; }
+    }
+}
